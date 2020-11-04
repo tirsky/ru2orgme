3918c386425576b7a128e914ba41e795d89425e2
Igor Tirsky <tirsky@gmail.com>
2020-11-05 01:46:41 +0300

    add autocomplite

--- ruorg-front/src/App.vue
+++ ruorg-front/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Find msg="Find Alternative"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Find from './components/Find'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Find
   }
 }
 </script>
---
+++ ruorg-front/src/components/Find.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="app">
+    <Autocomplete :search="search"
+                  @submit="handleSubmit"
+                  placeholder="Найти альтернативу..."></Autocomplete>
+  </div>
+</template>
+
+<script>
+
+import Autocomplete from '@trevoreyre/autocomplete-vue'
+import '@trevoreyre/autocomplete-vue/dist/style.css'
+
+export default {
+  name: 'Find',
+  components: {
+    Autocomplete
+  },
+  computed: {
+    noResults() {
+      return this.value && this.results.length === 0
+    }
+  },
+  methods: {
+    search(input) {
+      if (input.length < 1) {
+        return []
+      }
+      return ['http://ya.ru/', 'US'].filter(country => {
+        return country.toLowerCase()
+            .startsWith(input.toLowerCase())
+      })
+    },
+    handleSubmit(result) {
+      window.open(result)
+    }
+  }
+}
+
+
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+#app {
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+@media screen and (max-width: 600px) {
+  #app {
+    float: none;
+  }
+
+  #app {
+    float: none;
+    display: block;
+    text-align: left;
+    width: 100%;
+    margin: 0;
+    padding: 14px;
+  }
+
+  #app input[type=text] {
+    border: 1px solid #ccc;
+  }
+}
+
+</style>
